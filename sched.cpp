cab66155b7a5c47c4b0a6a3dcc0d1c2ff4834049
seoulsky-field <seoulsky.field02@gmail.com>
2024-04-07 22:38:53 +0900

    bug: not update remain_time in SRT

--- sched.cpp
+++ sched.cpp
@@ -12,13 +12,15 @@
 #include <algorithm>
 #include "sched.h"
 
+#include <chrono>
+#include <thread>
+
 class SPN : public Scheduler{
     private:
         Job search_shortest_job() {
             std::queue<Job> copy_job = job_queue_;
             std::queue<Job> tmp;
 
-            int index = 0;
             int min_time = 99999;
             Job shortest_job;
 
@@ -109,23 +111,90 @@ class RR : public Scheduler{
 
 class SRT : public Scheduler{
     private:
-        /*
-        * 구현 (멤버 변수/함수 추가 및 삭제 가능)
-        */
+        // Job search_SRT_job() {
+        //     int min_time = 99999;
+        //     Job shortest_job;
+
+        //     std::size_t size = job_queue_.size();
+
+        //     // Find the shortest remain_time Job
+        //     while (size-- > 0){
+        //         Job job_value = job_queue_.front();
+        //         job_queue_.pop();
+        //         job_queue_.push(job_value);
+
+        //         if (job_value.remain_time < min_time && job_value.arrival_time <= current_time_) {
+        //             min_time = job_value.remain_time;
+        //             shortest_job = job_value;
+        //         }
+        //     }
+
+        //     job_queue_ = tmp;
+
+        //     return shortest_job;
+        // }
+
+        void remove_done_job(const Job& current_job) {
+            std::queue<Job> tmp;
+
+            // Remove the remain_time == 0 Job in the job_queue_
+            while (!job_queue_.empty()) {
+                if (job_queue_.front().name != current_job.name) {
+                    tmp.push(job_queue_.front());
+                }
+                job_queue_.pop();
+            }
+
+            job_queue_ = tmp;
+        }
+
     public:
         SRT(std::queue<Job> jobs, double switch_overhead) : Scheduler(jobs, switch_overhead) {
             name = "SRT";
-            /*
-            * 위 생성자 선언 및 이름 초기화 코드 수정하지 말것.
-            * 나머지는 자유롭게 수정 및 작성 가능
-            */
         }
 
         int run() override {
-            /*
-            * 구현 
-            */
-            return -1;
+            if (current_job_.name == 0 && !job_queue_.empty()) {
+                current_job_ = job_queue_.front();
+            }
+
+            int prev_job_name = current_job_.name;
+
+            if (current_job_.remain_time == 0) {
+                current_job_.completion_time = current_time_;
+                end_jobs_.push_back(current_job_);
+                remove_done_job(current_job_);
+
+                if (job_queue_.empty()) return -1;
+
+                // current_job_ = search_SRT_job();
+                // current_time_ += switch_time_;
+            }
+            int min_time = 99999;
+            std::size_t size = job_queue_.size();
+
+            while (size-- > 0){
+                Job job_value = job_queue_.front();
+
+                if (job_value.remain_time < min_time && job_value.arrival_time <= current_time_) {
+                    min_time = job_value.remain_time;
+                    current_job_ = job_value;
+                }
+
+                job_queue_.pop();
+                job_queue_.push(job_value);
+            }
+
+            if (prev_job_name != current_job_.name) current_time_ += switch_time_;
+
+            if (current_job_.service_time == current_job_.remain_time) {
+                current_job_.first_run_time = current_time_;
+            }
+
+            current_time_++;
+            current_job_.remain_time--;
+
+            return current_job_.name;
         }
 };
 
